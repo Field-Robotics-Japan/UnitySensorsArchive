49a0684edb36fc3ad7dc6c937e671a3edf009ca7
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2022-09-03 14:54:20 +0900

    Update Camera.cs

--- Scripts/Runtime/Camera/Camera.cs
+++ Scripts/Runtime/Camera/Camera.cs
@@ -27,7 +27,10 @@ namespace FRJ.Sensor
         [SerializeField, Range(0.01f, 1000.0f)] private float _minDistance = 0.3f;
         [SerializeField, Range(0.01f, 1000.0f)] private float _maxDistance = 1000.0f;
 
+        [SerializeField] private float _scanRate = 20.0f;
+
         public Vector2Int resolution { get => this._resolution; }
+        public float scanRate { get => this._scanRate; }
 
         [Header("Informations(No need to input)")]
 
@@ -36,25 +39,10 @@ namespace FRJ.Sensor
         private RenderTexture _rt_depth = null;
 
         // Results
-        private ComputeBuffer _pointArray_x;
-        private ComputeBuffer _pointArray_y;
-        private ComputeBuffer _pointArray_z;
-        private ComputeBuffer _colorArray_r;
-        private ComputeBuffer _colorArray_g;
-        private ComputeBuffer _colorArray_b;
-        private float[] _pointArrayData_x;
-        private float[] _pointArrayData_y;
-        private float[] _pointArrayData_z;
-        private float[] _colorArrayData_r;
-        private float[] _colorArrayData_g;
-        private float[] _colorArrayData_b;
+        private ComputeBuffer _dataArrayCB;
+        private byte[] _dataArray;
 
-        public float[] pointArrayData_x { get => this._pointArrayData_x; }
-        public float[] pointArrayData_y { get => this._pointArrayData_y; }
-        public float[] pointArrayData_z { get => this._pointArrayData_z; }
-        public float[] colorArrayData_r { get => this._colorArrayData_r; }
-        public float[] colorArrayData_g { get => this._colorArrayData_g; }
-        public float[] colorArrayData_b { get => this._colorArrayData_b; }
+        public byte[] dataArray { get => this._dataArray; }
 
         [HideInInspector] public bool isInit = false;
 
@@ -87,42 +75,28 @@ namespace FRJ.Sensor
         public void Init()
         {
             if (isInit) return;
-            _rt_color = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32);
-            _rt_depth = new RenderTexture(Screen.width, Screen.height, 32, RenderTextureFormat.Depth);
+            _rt_color = new RenderTexture(_resolution.x, _resolution.y, 0, RenderTextureFormat.ARGB32);
+            _rt_depth = new RenderTexture(_resolution.x, _resolution.y, 32, RenderTextureFormat.Depth);
 
             _cam.SetTargetBuffers(_rt_color.colorBuffer, _rt_depth.depthBuffer);
 
-            _pointArrayData_x = new float[Screen.width * Screen.height];
-            _pointArrayData_y = new float[Screen.width * Screen.height];
-            _pointArrayData_z = new float[Screen.width * Screen.height];
-            _colorArrayData_r = new float[Screen.width * Screen.height];
-            _colorArrayData_g = new float[Screen.width * Screen.height];
-            _colorArrayData_b = new float[Screen.width * Screen.height];
-            _pointArray_x = new ComputeBuffer(_pointArrayData_x.Length, sizeof(float));
-            _pointArray_y = new ComputeBuffer(_pointArrayData_y.Length, sizeof(float));
-            _pointArray_z = new ComputeBuffer(_pointArrayData_z.Length, sizeof(float));
-            _colorArray_r = new ComputeBuffer(_colorArrayData_r.Length, sizeof(float));
-            _colorArray_g = new ComputeBuffer(_colorArrayData_g.Length, sizeof(float));
-            _colorArray_b = new ComputeBuffer(_colorArrayData_b.Length, sizeof(float));
+            _dataArray = new byte[_resolution.x * _resolution.y * 16];
+            _dataArrayCB = new ComputeBuffer(_resolution.x * _resolution.y * 4, sizeof(float));
 
             float n_inv = 1.0f / _cam.nearClipPlane;
             float f_inv = 1.0f / _cam.farClipPlane;
 
             _depthShader.SetFloat("n_f", n_inv - f_inv);
             _depthShader.SetFloat("f", f_inv);
-            _depthShader.SetInt("width", Screen.width);
-            _depthShader.SetInt("width_2",  Screen.width / 2);
-            _depthShader.SetInt("height_2", Screen.height / 2);
-            _depthShader.SetFloat("vDisW", Screen.width * 0.5f / Mathf.Tan(_horizontalFOV * 0.5f * Mathf.Deg2Rad));
-            _depthShader.SetFloat("vDisH", Screen.height * 0.5f / Mathf.Tan(_verticalFOV * 0.5f * Mathf.Deg2Rad));
+            _depthShader.SetInt("width", _resolution.x);
+            _depthShader.SetInt("width_2",  _resolution.x / 2);
+            _depthShader.SetInt("height_2", _resolution.y / 2);
+            _depthShader.SetFloat("vDisW", _resolution.x * 0.5f / Mathf.Tan(_horizontalFOV * 0.5f * Mathf.Deg2Rad));
+            _depthShader.SetFloat("vDisH", _resolution.y * 0.5f / Mathf.Tan(_verticalFOV * 0.5f * Mathf.Deg2Rad));
             _depthShader.SetTexture(0, "_depthBuffer", _rt_depth);
             _depthShader.SetTexture(0, "_colorBuffer", _rt_color);
-            _depthShader.SetBuffer(0, "_pointArray_x", _pointArray_x);
-            _depthShader.SetBuffer(0, "_pointArray_y", _pointArray_y);
-            _depthShader.SetBuffer(0, "_pointArray_z", _pointArray_z);
-            _depthShader.SetBuffer(0, "_colorArray_r", _colorArray_r);
-            _depthShader.SetBuffer(0, "_colorArray_g", _colorArray_g);
-            _depthShader.SetBuffer(0, "_colorArray_b", _colorArray_b);
+
+            _depthShader.SetBuffer(0, "_data", _dataArrayCB);
 
             isInit = true;
         }
@@ -136,21 +110,13 @@ namespace FRJ.Sensor
                 return;
             }
             if (!isInit) return;
-            Debug.Log(_pointArrayData_z[0]);
         }
-
         private void OnPostRender()
         {
             if (!Application.isPlaying || !isInit) return;
             _depthShader.Dispatch(0, _rt_depth.width / 8, _rt_color.height / 8, 1);
-            _pointArray_x.GetData(_pointArrayData_x);
-            _pointArray_y.GetData(_pointArrayData_y);
-            _pointArray_z.GetData(_pointArrayData_z);
-            _colorArray_r.GetData(_colorArrayData_r);
-            _colorArray_g.GetData(_colorArrayData_g);
-            _colorArray_b.GetData(_colorArrayData_b);
+            _dataArrayCB.GetData(_dataArray);
         }
-
     }
 }
 
